56fe7a8aa2d22bc130a3a68ec048e3baaf7da234
Jack Smentyna <djsmentya@gmail.com>
2011-10-17 01:20:11 +0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Interfreedom::Application.config.secret_token = '27eae486ed1e89c4649e74a26ed1bc8025c6bee99cf7a194cb48dc53f44fabb43828b89dcc1076956cfe3a515bfbc1981dd05731c50e4d7daf8186f479cb778f'
