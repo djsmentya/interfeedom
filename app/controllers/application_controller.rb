94cb965e2186f4cb7492f402a469dc14b7c11fa7
Jack Smentyna <djsmentya@gmail.com>
2011-10-18 19:28:34 +0300

    configured localization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,11 @@ class ApplicationController < ActionController::Base
 
   before_filter { |c| Authorization.current_user = c.current_user }
 
+  before_filter :set_locale
+
+  protected
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
 end
