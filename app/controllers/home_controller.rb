29b0c43fadce2619872c007a4894767ee3da57e2
Jack Smentyna <djsmentya@gmail.com>
2011-10-22 15:19:59 +0300

    should do lang switcher

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,4 +4,7 @@ class HomeController < ApplicationController
   def index
   end
 
+  def change_locale
+    I18n.locale = params[]#TODO add params
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -65,7 +65,12 @@ Interfreedom::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'home#index'
+  root :to => 'home#index' do
+    collection do
+      get 'change_locale'
+    end
+
+  end
 
   # See how all your routes lay out with "rake routes"
 
