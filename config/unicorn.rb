a8362688a7a7342abd84e53ce98a8ae56b912a15
Jack Smentyna <djsmentya@gmail.com>
2011-11-10 21:03:18 +0300

    fixed unicorn

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,7 +1,7 @@
 deploy_to  = "/usr/share/nginx/interfreedom"
 rails_root = "#{deploy_to}/current"
-pid_file   = "#{deploy_to}/shared/pids/unicorn.pid"
-socket_file= "#{deploy_to}/shared/unicorn.sock"
+pid_file   = "#{deploy_to}/tmp/pids/unicorn.pid"
+socket_file= "#{deploy_to}/tmp/unicorn.sock"
 log_file   = "#{rails_root}/log/unicorn.log"
 err_log    = "#{rails_root}/log/unicorn_error.log"
 old_pid    = pid_file + '.oldbin'
