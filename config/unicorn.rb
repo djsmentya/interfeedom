ced43d6f01ce9ecc8c31bca748db46157e56b507
Jack Smentyna <djsmentya@gmail.com>
2011-11-10 21:00:36 +0300

    fixed unicorn configs

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,4 +1,4 @@
-deploy_to  = "/usr/share/nginxinterfreedom"
+deploy_to  = "/usr/share/nginx/interfreedom"
 rails_root = "#{deploy_to}/current"
 pid_file   = "#{deploy_to}/shared/pids/unicorn.pid"
 socket_file= "#{deploy_to}/shared/unicorn.sock"
