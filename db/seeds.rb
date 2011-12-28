c54e8d02fdfa843dd9154f38bc0f0b2aab01f597
Jack Smentyna <djsmentya@gmail.com>
2011-12-28 22:30:22 +0300

    chenged seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'active_record'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -10,9 +11,9 @@
 # Product types
 puts 'working'
 puts 'Role creating...'
-  Role.create(:title => 'customer')
-  Role.create(:title => 'saler')
   puts 'Product type creating...'  
-  ProductType.create(:name=>'Video')
-  ProductType.create(:name=>'Audio')
+  ProductType.create!(:name=>'Video')
+  ProductType.create!(:name=>'Audio')
+  Role.create!(:title => 'customer')
+  Role.create!(:title => 'saler')
 puts 'finished'
