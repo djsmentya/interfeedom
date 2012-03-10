e0f11f8ad4ca9faceaaa27462fd94762d339d465
Jack Smentyna <djsmentya@gmail.com>
2012-03-10 20:07:32 +0200

    message system

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,7 @@
 class MessagesController < InheritedResources::Base
+  layout 'profile'
+
+  def index
+    @messages = Message.where(:to_user_id => current_user.id)
+  end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,4 @@
 class Message < ActiveRecord::Base
+  belongs_to :user
+  validates_presence_of :user_id, :to_user_id, :title, :body
 end
