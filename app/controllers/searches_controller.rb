bc6ea67c3bf26aa0359f6eeebfa6c7d455bae925
djsmentya <djsmentya@gmail.com>
2012-03-27 03:15:31 +0300

    fixes in serching

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -9,33 +9,35 @@ class SearchesController < ApplicationController
   end
 
   private #TODO Use some engines for search!!!!
-  def search(search_param = params[:search][:value])
-
-    condition_t = ['name LIKE ? or description LIKE ?']
-    values_t = ["%#{search_param}%", "%#{search_param}%"] 
+  def search()
     condition = ''
     values = []
+    unless params[:search].nil?
+      search_param = params[:search][:value]
+      condition_t = ['name LIKE ? or description LIKE ?']
+      values_t = ["%#{search_param}%", "%#{search_param}%"]
 
-    unless params[:search][:product_type].blank?
-      condition << ' and product_type = ? ' 
-      values << params[:search][:product_type]
-    end
-
-    unless params[:search][:price_from].blank?
-      if params[:search][:price_to].blank? 
-        condition << ' and price >= ? ' 
-        values << params[:search][:price_from]
-      else
-        condition << ' and (price between ? AND ?) ' 
-        values << params[:search][:price_from]
-        values << params[:search][:price_to]
+      unless params[:search][:product_type].blank?
+        condition << ' and product_type = ? '
+        values << params[:search][:product_type]
       end
-    end
 
-    scope = [condition.sub('and', '')] + values
+      unless params[:search][:price_from].blank?
+        if params[:search][:price_to].blank?
+          condition << ' and price >= ? '
+          values << params[:search][:price_from]
+        else
+          condition << ' and (price between ? AND ?) '
+          values << params[:search][:price_from]
+          values << params[:search][:price_to]
+        end
+      end
 
-    product_translations =  ProductTranslation.all(:select => :product_id ,:conditions => condition_t + values_t).collect{|translation| translation.product_id}
+      scope = [condition.sub('and', '')] + values
+
+      product_translations =  ProductTranslation.all(:select => :product_id ,:conditions => condition_t + values_t).collect{|translation| translation.product_id}
       @searches = Product.where('id in (?)', product_translations.uniq)
       @searches = @searches.where(scope).page(params[:page])
+    end
   end
 end
