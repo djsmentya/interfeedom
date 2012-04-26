811fb1b3c1c1e661db4a1d100eba00df29d074cf
djsmentya <djsmentya@gmail.com>
2012-04-27 01:59:45 +0300

    locale fixes

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -22,23 +22,33 @@ class SearchesController < ApplicationController
         condition << ' and product_type = ? '
         values << params[:search][:product_type]
       end
-
-      unless params[:search][:price_from].blank?
-        if params[:search][:price_to].blank?
-          condition << ' and price >= ? '
-          values << params[:search][:price_from]
-        else
-          condition << ' and (price between ? AND ?) '
-          values << params[:search][:price_from]
-          values << params[:search][:price_to]
+      if valid_integer?(params[:search][:price_to]) && valid_integer?(params[:search][:price_from])
+        unless  params[:search][:price_to].blank? && params[:search][:price_from].blank?
+          if  params[:search][:price_to].blank?
+            condition << ' and price >= ? '
+            values << params[:search][:price_from]
+          elsif params[:search][:price_from].blank?
+            condition << ' and price <= ? '
+            values << params[:search][:price_to]
+          else
+            condition << ' and (price between ? AND ?) '
+            values << params[:search][:price_from]
+            values << params[:search][:price_to]
+          end
         end
+      else
+        params[:search][:price_from] = nil
+        params[:search][:price_to] = nil
       end
-
       scope = [condition.sub('and', '')] + values
 
       product_translations =  ProductTranslation.all(:select => :product_id ,:conditions => condition_t + values_t).collect{|translation| translation.product_id}
       @searches = Product.where('id in (?)', product_translations.uniq)
       @searches = @searches.where(scope).page(params[:page])
     end
+  end
+  def valid_integer?(value)
+    converted = value.to_i.to_s
+    converted.eql?(value)
   end
 end
